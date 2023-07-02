d49cc4ad02ac49a1e73b8a88da003e1fcb267805
Danny Meyer <danny@xasopheno.com>
2023-07-02 00:16:09 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() -> Result<(), Error> {
         Pixels::new(WIDTH, HEIGHT, surface_texture)?
     };
 
-    let mut life = Grid::new_random(WIDTH as usize, HEIGHT as usize);
+    let mut life = Grid::new_alternating(WIDTH as usize, HEIGHT as usize);
 
     event_loop.run(move |event, _, control_flow| {
         // The one and only event that winit_input_helper doesn't have for us...
@@ -80,21 +80,6 @@ fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
     }
 }
 
-/// Generate a pseudorandom seed for the game's PRNG.
-fn generate_seed() -> (u64, u64) {
-    use byteorder::{ByteOrder, NativeEndian};
-    use getrandom::getrandom;
-
-    let mut seed = [0_u8; 16];
-
-    getrandom(&mut seed).expect("failed to getrandom");
-
-    (
-        NativeEndian::read_u64(&seed[0..8]),
-        NativeEndian::read_u64(&seed[8..16]),
-    )
-}
-
 const BIRTH_RULE: [bool; 9] = [false, false, false, true, false, false, false, false, false];
 const SURVIVE_RULE: [bool; 9] = [false, false, true, true, false, false, false, false, false];
 const INITIAL_FILL: f32 = 0.3;
@@ -102,10 +87,6 @@ const INITIAL_FILL: f32 = 0.3;
 #[derive(Clone, Copy, Debug, Default)]
 struct Cell {
     alive: bool,
-    // Used for the trail effect. Always 255 if `self.alive` is true (We could
-    // use an enum for Cell, but it makes several functions slightly more
-    // complex, and doesn't actually make anything any simpler here, or save any
-    // memory, so we don't)
     heat: u8,
 }
 
@@ -115,29 +96,19 @@ impl Cell {
     }
 
     #[must_use]
-    fn update_neibs(self, n: usize) -> Self {
-        let next_alive = if self.alive {
-            SURVIVE_RULE[n]
-        } else {
-            BIRTH_RULE[n]
-        };
-        self.next_state(next_alive)
+    fn update_neibs(self, heat: u8) -> Self {
+        self.next_state(heat)
     }
 
     #[must_use]
-    fn next_state(mut self, alive: bool) -> Self {
-        self.alive = alive;
-        if self.alive {
-            self.heat = 255;
-        } else {
-            self.heat = self.heat.saturating_sub(1);
-        }
+    fn next_state(mut self, heat: u8) -> Self {
+        self.heat = heat;
         self
     }
 
-    fn set_alive(&mut self, alive: bool) {
-        *self = self.next_state(alive);
-    }
+    // fn set_alivet(&mut self, alive: bool) {
+    // *self = self.next_state(alive);
+    // }
 
     fn cool_off(&mut self, decay: f32) {
         if !self.alive {
@@ -171,70 +142,37 @@ impl Grid {
         }
     }
 
-    fn new_random(width: usize, height: usize) -> Self {
+    fn new_alternating(width: usize, height: usize) -> Self {
         let mut result = Self::new_empty(width, height);
-        result.randomize();
+        result.fill_alternating();
         result
     }
 
-    fn randomize(&mut self) {
-        for (i, c) in self.cells.iter_mut().enumerate() {
-            *c = Cell::new(f32::sin(i as f32 * 1.0) > 0.4);
-        }
-        for _ in 0..3 {
-            self.update();
-        }
-        for c in self.cells.iter_mut() {
-            c.cool_off(0.4);
-        }
-    }
+    fn fill_alternating(&mut self) {
+        let values = (0..10).map(|x| x as f64 * 0.01);
+        let grid = (0..10).into_iter().map(|x| x as f32).collect::<Vec<_>>();
 
-    fn count_neibs(&self, x: usize, y: usize) -> usize {
-        let (xm1, xp1) = if x == 0 {
-            (self.width - 1, x + 1)
-        } else if x == self.width - 1 {
-            (x - 1, 0)
-        } else {
-            (x - 1, x + 1)
-        };
-        let (ym1, yp1) = if y == 0 {
-            (self.height - 1, y + 1)
-        } else if y == self.height - 1 {
-            (y - 1, 0)
-        } else {
-            (y - 1, y + 1)
-        };
-        self.cells[xm1 + ym1 * self.width].alive as usize
-            + self.cells[x + ym1 * self.width].alive as usize
-            + self.cells[xp1 + ym1 * self.width].alive as usize
-            + self.cells[xm1 + y * self.width].alive as usize
-            + self.cells[xp1 + y * self.width].alive as usize
-            + self.cells[xm1 + yp1 * self.width].alive as usize
-            + self.cells[x + yp1 * self.width].alive as usize
-            + self.cells[xp1 + yp1 * self.width].alive as usize
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let idx = x + y * self.width;
+                // Alternate based on row
+
+                let grid_idx = (y % grid.len()) as usize;
+                self.cells[idx] = Cell::new(f32::sin(y as f32 * x as f32 * 300.0) > 0.0);
+            }
+        }
     }
 
     fn update(&mut self) {
         for y in 0..self.height {
             for x in 0..self.width {
-                let neibs = self.count_neibs(x, y);
                 let idx = x + y * self.width;
-                let next = self.cells[idx].update_neibs(neibs);
+                let next = self.cells[idx];
                 // Write into scratch_cells, since we're still reading from `self.cells`
                 self.scratch_cells[idx] = next;
             }
         }
         std::mem::swap(&mut self.scratch_cells, &mut self.cells);
-    }
-
-    fn toggle(&mut self, x: isize, y: isize) -> bool {
-        if let Some(i) = self.grid_idx(x, y) {
-            let was_alive = self.cells[i].alive;
-            self.cells[i].set_alive(!was_alive);
-            !was_alive
-        } else {
-            false
-        }
     }
 
     fn draw(&self, screen: &mut [u8]) {
@@ -246,21 +184,6 @@ impl Grid {
                 [0, 0, c.heat, 0xff]
             };
             pix.copy_from_slice(&color);
-        }
-    }
-
-    fn set_line(&mut self, x0: isize, y0: isize, x1: isize, y1: isize, alive: bool) {
-        // probably should do sutherland-hodgeman if this were more serious.
-        // instead just clamp the start pos, and draw until moving towards the
-        // end pos takes us out of bounds.
-        let x0 = x0.clamp(0, self.width as isize);
-        let y0 = y0.clamp(0, self.height as isize);
-        for (x, y) in line_drawing::Bresenham::new((x0, y0), (x1, y1)) {
-            if let Some(i) = self.grid_idx(x, y) {
-                self.cells[i].set_alive(alive);
-            } else {
-                break;
-            }
         }
     }
 
