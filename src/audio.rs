366e392d1b3a27e7b6445b76c6e1401e7996b3c5
Danny Meyer <danny@xasopheno.com>
2023-07-14 22:29:05 -0400

    working

---
+++ src/audio.rs
@@ -0,0 +1,108 @@
+use crossbeam_channel as channel;
+use pa::{NonBlocking, Output, Stream};
+use pixels::Error;
+use portaudio as pa;
+use std::sync::{Arc, Mutex};
+use std::thread;
+
+pub fn spawn_audio(
+    buffer_size: usize,
+    filename: String,
+    audio_channel_sender: channel::Sender<Vec<f32>>,
+    audio_receiver: Arc<Mutex<channel::Receiver<Vec<f32>>>>,
+    sender_l: Arc<channel::Sender<Vec<f32>>>,
+    sender_r: Arc<channel::Sender<Vec<f32>>>,
+) -> Stream<NonBlocking, Output<f32>> {
+    let stream_handle =
+        spawn_audio_player(buffer_size, audio_receiver.clone(), sender_l, sender_r).unwrap();
+    _ = spawn_audio_reader(buffer_size, filename, audio_channel_sender);
+
+    stream_handle
+}
+
+fn spawn_audio_reader(
+    buffer_size: usize,
+    filename: String,
+    audio_channel_sender: channel::Sender<Vec<f32>>,
+) -> Result<(), ()> {
+    let mut reader = hound::WavReader::open(filename).unwrap();
+    let spec = reader.spec();
+    println!("{:?}", spec);
+
+    thread::spawn(move || {
+        let mut counter = 0;
+        let mut interleaved_buffer = vec![0.0; buffer_size * 2];
+
+        for sample in reader.samples::<f32>() {
+            let sample = sample.unwrap();
+            let index = counter % (buffer_size * 2);
+
+            interleaved_buffer[index] = sample;
+
+            counter += 1;
+
+            if counter % (buffer_size * 2) == 0 {
+                audio_channel_sender
+                    .send(interleaved_buffer.clone())
+                    .unwrap();
+            }
+        }
+    });
+
+    Ok(())
+}
+
+fn spawn_audio_player(
+    buffer_size: usize,
+    audio_receiver: Arc<Mutex<channel::Receiver<Vec<f32>>>>,
+    sender_l: Arc<channel::Sender<Vec<f32>>>,
+    sender_r: Arc<channel::Sender<Vec<f32>>>,
+) -> Result<Stream<NonBlocking, Output<f32>>, ()> {
+    let pa = pa::PortAudio::new().unwrap();
+    let output_stream_settings = get_output_settings(buffer_size, &pa).unwrap();
+
+    let mut stream = pa
+        .open_non_blocking_stream(
+            output_stream_settings,
+            move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
+                let r_audio_lock = audio_receiver.lock().unwrap();
+                let audio_data = (*r_audio_lock)
+                    .recv()
+                    .unwrap_or_else(|_| vec![0.0; frames * 2]); // *2 for stereo
+
+                sender_l
+                    .send(audio_data.iter().step_by(2).cloned().collect())
+                    .unwrap();
+                sender_r
+                    .send(audio_data.iter().skip(1).step_by(2).cloned().collect())
+                    .unwrap();
+
+                for (frame, chunk) in audio_data.chunks_exact(2).enumerate() {
+                    let index = frame * 2;
+                    buffer[index] = chunk[0];
+                    buffer[index + 1] = chunk[1];
+                }
+
+                pa::Continue
+            },
+        )
+        .unwrap();
+
+    stream.start().unwrap();
+
+    Ok(stream)
+}
+
+fn get_output_settings(
+    buffer_size: usize,
+    pa: &pa::PortAudio,
+) -> Result<pa::stream::OutputSettings<f32>, Error> {
+    let def_output = pa.default_output_device().unwrap();
+    let output_info = pa.device_info(def_output).unwrap();
+    let latency = output_info.default_low_output_latency;
+    let output_params = pa::StreamParameters::new(def_output, 2, true, latency);
+
+    let output_settings = pa::OutputStreamSettings::new(output_params, 48000.0, buffer_size as u32);
+
+    Ok(output_settings)
+}
